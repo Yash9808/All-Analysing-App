62599aa8da588e8a458a79eb81af320f88115bdc
Yash sharma <44648735+Yash9808@users.noreply.github.com>
2025-03-07 19:35:33 +0000

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -4,5 +4,5 @@ st.title("Main Streamlit App")
 
 st.write("Click below to open one of the apps:")
 
-st.markdown("[📌 Open Transcribe-And-Analyse](https://transcribe-analyse.streamlit.app)", unsafe_allow_html=True)
-st.markdown("[📌 Open Call Analysis V4](https://call-analysis-v4one.streamlit.app)", unsafe_allow_html=True)
+st.markdown("[📌 Open Transcribe-And-Analyse](https://yp2opqdw5du4x4cevfryet.streamlit.app/)", unsafe_allow_html=True)
+st.markdown("[📌 Open Call Analysis V4](https://a2rpeak3gt2tflgd4skjvv.streamlit.app/)", unsafe_allow_html=True)
